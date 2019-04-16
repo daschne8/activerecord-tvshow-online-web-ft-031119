a3e17d64f4475664e96322ea83f77b9f3cce63ca
Daniel Schneider <daschne8@gmail.com>
2019-04-16 20:00:27 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,15 +4,15 @@ class Show < ActiveRecord::Base
     self.all.maximum(:rating)
   end
 
-  def most_popular_show
+  def self.most_popular_show
     self.all.maximum(:rating)
   end
 
-  def lowest_rating
+  def self.lowest_rating
     self.all.minimum(:rating)
   end
 
-  def least_popular_show
+  def self.least_popular_show
     self.all.minimum(:rating)
   end
 
